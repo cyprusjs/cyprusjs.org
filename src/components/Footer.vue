b3a3b8637ecc87c7d1ebdde0173684210af7da05
Jonas Broms <jonas.broms@gmail.com>
2019-07-22 04:27:14 +0300

    fix: Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -68,7 +68,7 @@
     <section class="section">
       <div class="container">
         <div class="column">
-          <p class="is-size-7">Copyright © 2019 CyprusJS. All rights reserved.</p>
+          <p class="is-size-7">Copyright © 2019 to the Authors of CyprusJS. All rights reserved. The source code is licensed <a href="https://github.com/cyprusjs/cyprusjs.org/LICENSE" target="_blank">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a></p>
         </div>
       </div>
     </section>
