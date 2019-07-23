cca0113c8e81057713bdd42fc9f31cdccc4bc97b
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:41:07 +0300

    feat: Create Chapters.vue

---
+++ src/components/Chapters.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="section" id="what-we-do">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-full">
+          <h3 class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold">Chapters</h3>
+        </div>
+      </div>
+      <div class="columns">
+        <article class="column is-6">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Nicosia</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        </article>
+        <article class="column is-6">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Limassol</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        </article>
+      </div>
+      <div class="divider"></div>
+      <div class="columns">
+        <article class="column is-6">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Paphos</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        </article>
+        <article class="column is-6">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Larnaca</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Chapters'
+}
+</script>
+
+<style scoped>
+</style>
