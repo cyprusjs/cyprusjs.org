3e572b2b6e62043887fa1f450a932d057cec9f7b
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:52:08 +0300

    fix: Update Column width

--- src/components/WhatWeDo.vue
+++ src/components/WhatWeDo.vue
@@ -7,33 +7,33 @@
         </div>
       </div>
       <div class="columns">
-        <article class="column is-6">
+        <article class="column is-offset-1 is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Venue</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Our amazing partners are the ones who make it possible for us to host our tech meetups. They help us with a physical space to accommondate the event. In return we help them by marketing their services & products too the event attendees.</p>
         </article>
-        <article class="column is-6">
+        <article class="column is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Responsibility</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Our community events are made by and for the attendees. As a comfort for our partners a senior team member will take responsibility of making sure that the venue is left in the same condition as we found it in.</p>
         </article>
       </div>
       <div class="divider"></div>
       <div class="columns">
-        <article class="column is-6">
+        <article class="column is-offset-1 is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Format</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">We have a well structured format for our events that are made to inspire our attendees and spark discussions. Our format has been iteratated and proved its value for all parties involved thanks to the time Patrick spent founding <a href="http://barcelonajs.org/" target="_blank">BarceloneJS</a>. For more information about our format, <a href="https://github.com/cyprusjs/CyprusJS/blob/master/PLAYBOOK.md" target="_blank">see our playbook</a>.</p>
         </article>
-        <article class="column is-6">
+        <article class="column is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Lectures</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">We provide a physical platform for hackers, programmers & tech people to meet, talk, share ideas and connect. Anyone can propose subjects or sign up to be a speaker by adding a propasal or request to our <a href="https://github.com/cyprusjs/talks/issues/new/choose" target="_blank">Github</a> or contact us on our <a href="https://twitter.com/CyprusJS" target="_blank">Twitter</a>.</p>
         </article>
       </div>
       <div class="divider"></div>
       <div class="columns">
-        <article class="column is-6">
+        <article class="column is-offset-1 is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Socializing</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Our event schedule is created so there is plenty of time to network and socialize both before and after the public talks. We try to create a relaxed enviroment in which socializing with all attendees and speakers comes naturally.</p>
         </article>
-        <article class="column is-6">
+        <article class="column is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Car Pooling</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Through our social media channels, foremost our <a href="https://github.com/cyprusjs/CyprusJS/issues" target="_blank">Github</a> we encourage and try to help out with car pooling from all different cities within Cyprus so you don't have to miss an event just because it's hosted in a different city. Easy, fun and a good option for the enviroment.</p>
         </article>
