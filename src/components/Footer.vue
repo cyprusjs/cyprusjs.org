b3ebe8df7f9ec95a275fc37daa7ceaf3f3cf24e4
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:42:26 +0300

    feat: Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,52 +1,64 @@
 <template>
-  <footer class="footer" aria-label="page footer">
-
+  <footer class="footer" id="pagefooter" aria-label="page footer">
     <section class="section">
       <div class="container">
         <div class="columns">
-
           <article class="column">
-            <h5 class="title is-5">Get in touch</h5>
+            <h5 class="title is-5">Chapters</h5>
             <nav>
               <ul class="menu-list">
                 <li>
-                  <a href="https://twitter.com/cyprusjs" target="_blank">Twitter</a>
+                  <a href="https://ti.to/cyprusjs/cyprusjs-nicosia" target="_blank">
+                    <i class="fab fa-home"></i> Nicosia
+                  </a>
                 </li>
                 <li>
-                  <a href="https://github.com/cyprusjs/cyprusjs.org" target="_blank">GitHub</a>
+                  <a href="https://ti.to/cyprusjs/cyprusjs-limassol" target="_blank">
+                    <i class="fab fa-home"></i> Limassol
+                  </a>
                 </li>
                 <li>
-                  <a href="https://www.facebook.com/Cyprusjs-431172884395749/" target="_blank">Facebook</a>
+                  <a href="https://ti.to/cyprusjs/cyprusjs-paphos" target="_blank">
+                    <i class="fab fa-home"></i> Paphos
+                  </a>
                 </li>
                 <li>
-                  <a href="https://github.com/cyprusjs/cyprusjs.org" target="_blank">Instagram</a>
+                  <a href="https://ti.to/cyprusjs/cyprusjs-larnaca" target="_blank">
+                    <i class="fab fa-home"></i> Larnaca
+                  </a>
                 </li>
               </ul>
             </nav>
           </article>
-
           <article class="column">
             <h5 class="title is-5">The Team</h5>
             <nav>
               <ul class="menu-list">
                 <li>
-                  <a href="https://twitter.com/PatrickHeneise" target="_blank">Patrick Heneise</a>
+                  <a href="https://twitter.com/PatrickHeneise" target="_blank">
+                    <i class="fab fa-user-circle"></i> Patrick Heneise
+                  </a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/jonasbroms" target="_blank">Jonas Bröms</a>
+                  <a href="https://twitter.com/jonasbroms" target="_blank">
+                    <i class="fab fa-user-circle"></i> Jonas Bröms
+                  </a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/alexandr_bbm" target="_blank">Alexander Gazizov</a>
+                  <a href="https://twitter.com/alexandr_bbm" target="_blank">
+                    <i class="fab fa-user-circle"></i> Alexander Gazizov
+                  </a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/elgin_ali" target="_blank">Ali Elgin</a>
+                  <a href="https://twitter.com/elgin_ali" target="_blank">
+                    <i class="fab fa-user-circle"></i> Ali Elgin
+                  </a>
                 </li>
               </ul>
             </nav>
           </article>
-
           <article class="column">
-            <h5 class="title is-5">Supporters</h5>
+            <h5 class="title is-5">Partners</h5>
             <nav>
               <ul class="menu-list">
                 <li>
@@ -56,16 +68,58 @@
                   <a href="http://www.ariscy.com/" target="_blank">ARIS</a>
                 </li>
                 <li>
+                  <a href="https://www.facebook.com/FutureWorldsCenter/" target="_blank">Future Worlds Center</a>
+                </li>
+                <li>
                   <a href="https://www.odduse.com/" target="_blank">Odduse</a>
                 </li>
               </ul>
             </nav>
           </article>
+          <article class="column" id="socialmedia">
+            <h5 class="title is-5">Get in touch</h5>
+            <nav class="level is-mobile">
+              <div class="level-item has-text-centered">
+                <div>
+                  <a class="has-text-centered" href="https://twitter.com/cyprusjs" target="_blank" aria-label="CyprusJS Twitter Page">
+                    <i class="fab fa-twitter fa-2x"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <a href="https://github.com/cyprusjs/cyprusjs.org" target="_blank" aria-label="CyprusJS LinkedIn Page">
+                    <i class="fab fa-github fa-2x"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <a href="https://www.facebook.com/Cyprusjs-431172884395749/" target="_blank" aria-label="CyprusJS Facebook Page">
+                    <i class="fab fa-facebook fa-2x"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <a href="https://github.com/cyprusjs/cyprusjs.org" target="_blank" aria-label="CyprusJS Instagram Page">
+                    <i class="fab fa-instagram fa-2x"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <a href="https://www.linkedin.com/groups/12274238/" target="_blank" aria-label="CyprusJS LinkedIn Page">
+                    <i class="fab fa-linkedin fa-2x"></i>
+                  </a>
+                </div>
+              </div>
+            </nav>
+          </article>
         </div>
       </div>
     </section>
-
-    <section class="section">
+    <section class="section" id="pagesocket" aria-label="page socket">
       <div class="container">
         <div class="column">
           <p class="is-size-7">Copyright © 2019 to the Authors of CyprusJS. All rights reserved. The source code is licensed <a href="https://github.com/cyprusjs/cyprusjs.org/LICENSE" target="_blank">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a></p>
