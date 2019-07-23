2bc0f087a4280ba419f56603f263b928c3b12023
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:46:57 +0300

    BREAKING CHANGE: Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header />
+    <v-navbar />
     <router-view />
     <v-subscribe />
     <v-footer />
@@ -9,11 +9,31 @@
 
 <script>
 import Vue from 'vue'
-import Header from './components/Header.vue'
+import Navbar from './components/Navbar.vue'
+import Pageheader from './components/PageHeader.vue'
+import Eventsection from './components/EventSection.vue'
+import Ourbeliefs from './components/OurBeliefs.vue'
+import Whoweare from './components/WhoWeAre.vue'
+import Whatwedo from './components/WhatWeDo.vue'
+import Ourteam from './components/OurTeam.vue'
+import Chapters from './components/Chapters.vue'
+import Speaker from './components/Speaker.vue'
+import Partner from './components/Partner.vue'
+import Conduct from './components/Conduct.vue'
 import Subscribe from './components/Subscribe.vue'
 import Footer from './components/Footer.vue'
 
-Vue.component('v-header', Header)
+Vue.component('v-navbar', Navbar)
+Vue.component('v-pageheader', Pageheader)
+Vue.component('v-eventsection', Eventsection)
+Vue.component('v-ourbeliefs', Ourbeliefs)
+Vue.component('v-whatwedo', Whatwedo)
+Vue.component('v-whoweare', Whoweare)
+Vue.component('v-ourteam', Ourteam)
+Vue.component('v-chapters', Chapters)
+Vue.component('v-speaker', Speaker)
+Vue.component('v-partner', Partner)
+Vue.component('v-conduct', Conduct)
 Vue.component('v-footer', Footer)
 Vue.component('v-subscribe', Subscribe)
 
