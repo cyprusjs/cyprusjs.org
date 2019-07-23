a188aa5ebfd591457408b7a5a5ff140c51b11b8f
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:43:47 +0300

    feat: Create OurTeam.vue

---
+++ src/components/OurTeam.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="section" id="our-team">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-full">
+          <h3 class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold">Our Team</h3>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-3">
+          <article class="">
+            <figure class="media-left">
+              <p class="image is-128x128">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+              </p>
+            </figure>
+            <div class="">
+              <div class="content">
+                <h5>John Smith</h5>
+                <p>Paphos</p>
+              </div>
+            </div>
+          </article>
+        </div>
+
+        <div class="column is-3">
+
+        </div>
+
+        <div class="column is-3">
+
+        </div>
+
+        <div class="column is-3">
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Ourteam'
+}
+</script>
+
+<style scoped></style>
+
+
+<article class="media">
+
+</article>
