a90c9e2aab109b7d67b45fc8f90cbee1a4fa8f39
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:43:21 +0300

    feat: Create OurBeliefs.vue

---
+++ src/components/OurBeliefs.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="section" id="about-us">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-full">
+          <h3 class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold">Our Beliefs</h3>
+        </div>
+      </div>
+      <div class="columns">
+        <article class="column is-4">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Mission</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Provide great tech events and meetups for Javascript coders in Cyprus.</p>
+        </article>
+        <article class="column is-4">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Vision</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Become the number one event organizer in Cyprus for JS developers.</p>
+        </article>
+        <article class="column is-4">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Values</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Connect, Innovate, Educate, Inspire, Motivate, Cultivate & being kind.</p>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Aboutus'
+}
+</script>
+
+<style scoped></style>
