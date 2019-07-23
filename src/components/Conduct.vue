ca5680e1bffec6338216424b25d038a2f3f3e9a0
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:41:21 +0300

    feat: Create Conduct.vue

---
+++ src/components/Conduct.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section" id="code-of-conduct" aria-label="Code of Conduct">
+    <div class="container">
+      <div class="columns is-vcentered">
+        <div class="column is-5">
+          <h4 class="is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold" id="code-conduct">Code of Conduct</h4>
+          <p class="is-size-4-desktop is-size-5-tablet is-size-6-mobile">
+            Any harmful or discriminating behaviour
+            will not be tolerated and results in the offending person being
+            expelled from the meetup.
+          </p>
+          <p class="is-size-4-desktop is-size-5-tablet is-size-6-mobile">
+            For details on what kinds of behaviour are not tolerated and
+            consequences for violating these rules, we refer to the
+            <a href="https://rubyberlin.github.io/code-of-conduct">Berlin Code of Conduct.</a>
+          </p>
+        </div>
+        <div class="column is-7">
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Conduct'
+}
+</script>
+
+<style scoped></style>
