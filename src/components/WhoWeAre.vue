81836e14dc56c9b434844099f0b07fffc6f3ae6b
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:44:51 +0300

    feat: Create WhoWeAre.vue

---
+++ src/components/WhoWeAre.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section" id="who-we-are">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-full">
+          <h3 class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold">Who We Are</h3>
+        </div>
+      </div>
+      <div class="columns">
+        <article class="column is-offset-1 is-10">
+          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Community & Event Organizer</h4>
+          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">We are a community & an event organizer that focuses on innovation, precision and relationship.
+          We aim to be an awesome, inclusive and safe community that hosts tech meetups
+          where people meet, hang out, chat, listen to talks, exchange ideas and make new friends.</p>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Whoweare'
+}
+</script>
+
+<style scoped>
+</style>
