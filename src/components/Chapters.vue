f9bec31f21ccf0dfee2532a8ff76c33e960e86cb
Jonas Broms <jonas.broms@gmail.com>
2019-07-23 23:52:16 +0300

    fix: Update Column width

--- src/components/Chapters.vue
+++ src/components/Chapters.vue
@@ -7,22 +7,22 @@
         </div>
       </div>
       <div class="columns">
-        <article class="column is-6">
+        <article class="column is-offset-1 is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Nicosia</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
         </article>
-        <article class="column is-6">
+        <article class="column is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Limassol</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
         </article>
       </div>
       <div class="divider"></div>
       <div class="columns">
-        <article class="column is-6">
+        <article class="column is-offset-1 is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Paphos</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
         </article>
-        <article class="column is-6">
+        <article class="column is-5">
           <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Larnaca</h4>
           <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
         </article>
