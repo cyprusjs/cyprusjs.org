6b1e108973698dda0764e27e8b99aebb4eefbaf3
Jonas Broms <jonas.broms@gmail.com>
2019-07-27 23:54:45 +0300

    fix: Graphical update

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -1,15 +1,16 @@
 <template>
-  <section class="section" id="partner">
+  <section class="section has-background-dark-gradient" id="partner">
     <div class="container">
       <div class="columns is-vcentered">
         <div class="column is-7">
-          <figure class="image is-4by3">
+          <figure class="image is-4by3 is-hidden-mobile">
             <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
           </figure>
         </div>
         <div class="column is-5">
-          <h4 class="is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">We need your help!</h4>
-          <p class="is-size-4-desktop is-size-5-tablet is-size-6-mobile">
+          <h3 class="is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold has-text-light">Become a Partner</h3>
+          <h4 class="is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold has-text-lightest">We need your help!</h4>
+          <p class="is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-lighter">
             We organise events regularly across cities in Cyprus; such as Nicosia, Limassol, Paphos and Larnaca. We do this free of charge for attendees and in the true spirit of <a href="https://opensource.org/" target="_blank">Open-Source</a> philosophy.
             Our community is run by enthusiastic volunteers. If you can help us organise/host an event, please reach out to our <a href="https://twitter.com/CyprusJS" target="_blank">Twitter</a> or contact us on our <a href="mailto:info@cyprusjs.org?subject=I would like to host a CyprusJS event">Email</a>.
           </p>
