f190f41e5405df417324bfff28f21c8af64b8d55
Jonas Broms <jonas.broms@gmail.com>
2019-07-27 23:54:31 +0300

    fix: Graphical update

--- src/components/WhoWeAre.vue
+++ src/components/WhoWeAre.vue
@@ -1,15 +1,15 @@
 <template>
-  <section class="section" id="who-we-are">
+  <section class="section has-background-dark-gradient" id="who-we-are">
     <div class="container">
       <div class="columns">
-        <div class="column is-full">
-          <h3 class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold">Who We Are</h3>
+        <div class="column is-offset-1-desktop is-10-tablet is-12-tablet">
+          <h3 class="is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-weight-semibold has-text-light">Who We Are</h3>
         </div>
       </div>
       <div class="columns">
-        <article class="column is-offset-1 is-10">
-          <h4 class="has-text-centered has-text-left-mobile is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold">Community & Event Organizer</h4>
-          <p class="has-text-centered has-text-left-mobile is-size-4-desktop is-size-5-tablet is-size-6-mobile">We are a community & an event organizer that focuses on innovation, precision and relationship.
+        <article class="column is-offset-1-desktop is-10-desktop is-12-tablet">
+          <h4 class="is-size-2-desktop is-size-3-tablet is-size-4-mobile has-text-weight-semibold has-text-lightest">Community & Event Organizer</h4>
+          <p class="is-size-4-desktop is-size-5-tablet is-size-6-mobile has-text-lighter">We are a community & an event organizer that focuses on innovation, precision and relationship.
           We aim to be an awesome, inclusive and safe community that hosts tech meetups
           where people meet, hang out, chat, listen to talks, exchange ideas and make new friends.</p>
         </article>
@@ -24,5 +24,4 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
