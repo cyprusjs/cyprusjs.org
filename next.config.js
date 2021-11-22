f70c9912cbb6603a5c4a1f35e15fcb0c3d942d53
Patrick Heneise <patrick@zentered.co>
2021-11-22 15:27:09 +0200

    fix: use new swc for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = {
+  swcMinify: true,
+  trailingSlach: true,
   reactStrictMode: true,
   webpack: (config) => {
     config.plugins.push(new WindiCSSWebpackPlugin())
